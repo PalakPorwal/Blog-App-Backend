e0d94fe2807cc7134aca58b991bfbcf30f5adca6
Madhav <madhavpaliwal02@gmail.com>
2023-09-08 19:33:47 +0530

    Code Optimized

--- blog-app-apis/src/main/java/com/palak/config/SecurityConfig.java
+++ blog-app-apis/src/main/java/com/palak/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.palak.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +10,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -32,8 +25,6 @@ public class SecurityConfig {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
-
-    
 
     // @Bean
     // public FilterRegistrationBean corsFilter() {
@@ -86,23 +77,6 @@ public class SecurityConfig {
                 .httpBasic(withDefaults());
         return http.build();
     }
-
-    // public SecurityConfig(DataSource dataSource) {
-    // this.dataSource = dataSource;
-    // }
-
-    // @Bean
-    // public UserDetailsManager users(DataSource dataSource) {
-    // PasswordEncoder passwordEncoder = passwordEncoder();
-    // UserDetails user = User.builder()
-    // .username("user")
-    // .password(passwordEncoder.encode("password"))
-    // .roles("USER")
-    // .build();
-    // JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-    // users.createUser(user);
-    // return users;
-    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
